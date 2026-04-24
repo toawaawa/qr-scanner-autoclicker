5f45b86263bf8b3f86c854a907894eb3de95759b
toawaawa <totolwto@gmail.com>
2026-04-24 16:11:56 +0800

    add telegram bot connection

--- scanner.py
+++ scanner.py
@@ -1,3 +1,5 @@
+import os
+
 import PIL.ImageGrab
 import PIL.Image
 import cv2
@@ -6,6 +8,28 @@ import webbrowser
 import time
 import datetime
 import pyautogui
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get the secrets
+TOKEN = os.getenv("TELEGRAM_API")
+CHAT_ID = os.getenv("TELEGRAM_ID")
+
+def send_telegram_msg(message):
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    try:
+        requests.post(url, data=payload)
+    except Exception as e:
+        print(f"Failed to send Telegram message: {e}")
 
 def is_color_near(current_color, target_color, tolerance=10):
     """
@@ -39,9 +63,9 @@ def scan_screen_with_opencv(click_x, click_y, x1, y1, x2, y2):
             screenshot_click = PIL.ImageGrab.grab(bbox=(click_x, click_y, click_x + 2, click_y + 2)).convert('RGB')
             r, g, b = screenshot_click.getpixel((0, 0))
             print(r, g, b)
-
             if is_color_near((r, g, b), clickable_color) and clicked == False:
                 print(datetime.datetime.now(),":button clickable")
+                send_telegram_msg(str(datetime.datetime.now()) + "\nbutton clickable")
                 pyautogui.click(click_x,click_y,60,0.05)
                 clicked = True
                 sleep_time = 1
@@ -64,6 +88,7 @@ def scan_screen_with_opencv(click_x, click_y, x1, y1, x2, y2):
             if data:
                 if data != last_url:
                     print(datetime.datetime.now(),":qr code appeared")
+                    send_telegram_msg(str(datetime.datetime.now()) + "\nqr code appeared")
                     webbrowser.open(data)
                     time.sleep(20)
                     # switch back to chrome page
